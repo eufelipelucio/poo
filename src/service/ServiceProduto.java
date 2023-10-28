141e12dab3e5d7e9cd2fc613654dbcaac85daf4f
Felipe <felipesou94@gmail.com>
2023-10-28 15:40:30 -0300

    refatorando service try catch

--- src/service/ServiceProduto.java
+++ src/service/ServiceProduto.java
@@ -15,39 +15,33 @@ public class ServiceProduto {
 	}
 	 
 	public String inserirProduto(Produto produto) {
-		 
+	 
 		 try{
 			 produtos.add(produto);
+			 return "Produto adicionado com sucesso!";
 		 }catch(Exception erro){
 			 return "Erro ao adicionar produto!";
-		 }finally {
-			 return "Produto adicionado com sucesso!";
-		 }
-		 
+		 }	 
 	 }
 	
 	public Produto pegarProduto(int posicao) {
 		Produto produto = produtos.get(posicao);;
 		return produto;
 	}
-	public String alterarProduto(int posicao,Produto produto) {
+	public String alterarProduto(int posicao, Produto produto) {
 		try {
 			produtos.set(posicao, produto);
-		}catch(Exception erro) {
+			return "Produto alterado com sucesso!";
+		} catch (Exception erro) {
 			return "Erro ao alterar Produto!";
-		}finally{
-			return"Produto alterado com sucesso!";
 		}
 	}
 	public String removerProduto(int posicao) {
-		 
-		 try{
-			 produtos.remove(posicao);
-		 }catch(Exception erro){
-			 return "Erro ao remover Produto";
-		 }finally {
-			 return "Produto removido com sucesso";
-		 }
-		 
-	 }
+		try {
+			produtos.remove(posicao);
+			return "Produto removido com sucesso";
+		} catch (Exception erro) {
+			return"Erro ao remover Produto";
+		}
+	}
 }
